a71f75fb2ac75de5852e501f545bf52a6b536e0a
eomjengsig <travelereom@naver.com>
2022-10-04 22:20:26 +0900

    chatapp

--- src/server.js
+++ src/server.js
@@ -1,5 +1,41 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { type } from "os";
 
 const app = express();
 
-app.listen(4000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`✅Listening on http://localhost:4000/`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "익명";
+  console.log("✅Connected to Browser");
+  socket.on("close", () => console.log("❌Disconnected from Browser"));
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message);
+    switch (parsed.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${parsed.payload.toString()}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = parsed.payload;
+        break;
+    }
+  });
+});
+
+server.listen(4000, handleListen);
